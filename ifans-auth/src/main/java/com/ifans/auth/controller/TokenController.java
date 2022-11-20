f93ec57c06baa78edae9528e563e3b6f3bd7558e
hjx <hjxsuper@foxmail.com>
2022-11-20 16:51:23 +0800

    权限认证模块

---
+++ ifans-auth/src/main/java/com/ifans/auth/controller/TokenController.java
@@ -0,0 +1,52 @@
+package com.ifans.auth.controller;
+
+import com.ifans.api.system.model.LoginUser;
+import com.ifans.auth.domain.LoginBody;
+import com.ifans.auth.service.SysLoginService;
+import com.ifans.common.auth.AuthUtil;
+import com.ifans.common.domaain.R;
+import com.ifans.common.service.TokenService;
+import com.ifans.common.utils.JwtUtils;
+import com.ifans.common.utils.SecurityUtils;
+import com.ifans.common.utils.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * token控制层
+ */
+@RestController
+public class TokenController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private SysLoginService sysLoginService;
+
+    @PostMapping("login")
+    public R<?> login(@RequestBody LoginBody form) {
+        // 用户登录
+        LoginUser userInfo = sysLoginService.login(form.getUsername(), form.getPassword());
+        // 获取登录token
+        return R.ok(tokenService.createToken(userInfo));
+    }
+
+    @DeleteMapping("logout")
+    public R<?> logout(HttpServletRequest request) {
+        String token = SecurityUtils.getToken(request);
+        if (StringUtils.isNotEmpty(token)) {
+            String username = JwtUtils.getUserName(token);
+            // 删除用户缓存记录
+            AuthUtil.logoutByToken(token);
+            // 记录用户退出日志
+            sysLoginService.logout(username);
+        }
+        return R.ok();
+    }
+}
